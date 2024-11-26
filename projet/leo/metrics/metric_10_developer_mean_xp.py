1fed8c5feddbfac87c5c12682503e4b1b61bde9b
moshi <leofornoff02@gmail.com>
2024-11-25 21:15:04 -0500

    Metrics 10, 11

--- projet/leo/metrics/metric_10_developer_mean_xp.py
+++ projet/leo/metrics/metric_10_developer_mean_xp.py
@@ -4,7 +4,7 @@ from Objects.Version import Version
 def developer_count_version(version: Version):
     developer_xp: dict = dict()
     for commit in version.getCommitsLogs():
-        developer = commit.getAuthor()
+        developer = commit.author.email
         if developer not in developer_xp:
             developer_xp[developer] = 0
         developer_xp[developer] += 1
@@ -13,12 +13,14 @@ def developer_count_version(version: Version):
 
 def metric_10_developer_mean_xp(file: str, version: Version):
     previous_version = version.getPreviousVersion()
+    if file not in version.getDiff():
+        return 0
     commits = version.getDiff()[file]['commits']
     developer_xp = developer_count_version(previous_version)
     mean = 0
 
     for commit in commits:
-        developer = commit.getAuthor()
+        developer = commit.author.email
         if developer in developer_xp:
             mean += developer_xp[developer]
 
--- projet/leo/metrics/metric_11_developer_min_xp.py
+++ projet/leo/metrics/metric_11_developer_min_xp.py
@@ -4,12 +4,14 @@ from projet.leo.metrics.metric_10_developer_mean_xp import developer_count_versi
 
 def metric_11_developer_min_xp(file: str, version: Version):
     previous_version = version.getPreviousVersion()
+    if file not in version.getDiff():
+        return 0
     commits = version.getDiff()[file]['commits']
     developer_xp = developer_count_version(previous_version)
     min_xp = float('inf')
 
     for commit in commits:
-        developer = commit.getAuthor()
+        developer = commit.author.email
         if developer in developer_xp:
             min_xp = min(min_xp, developer_xp[developer])
 
